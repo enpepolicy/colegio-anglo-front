5fe8d6ee976b0a72eb538b707badaff1c51acb45
Enrique <enriquep36@hotmail.com>
2019-07-19 03:46:32 -0500

    ready

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
+import Vuetify from 'vuetify'
 
 Vue.use(VueRouter)
 Vue.use(Vuetify)
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
 // vue.config.js
 module.exports = {
-  outputDir : 'docs',
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/colegio-anglo-front/'
-    : '/'
+  outputDir : 'docs'
 }
